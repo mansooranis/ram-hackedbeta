830fd8e68564ea1af445a6f8f79373a66e923848
mansooranis <mansooranis60@gmail.com>
2021-11-13 20:39:29 -0700

    frontend i

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import HomePage from './Pages/HomePage/Components/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <h3 className="h">Hello world</h3>
+      <HomePage name = "mansoor"/>
+      <HomePage name= "armaan"/>
+      <HomePage/>
+      <HomePage/>
     </div>
   );
 }
---
+++ frontend/src/Pages/HomePage/Components/HomePage.js
@@ -0,0 +1,7 @@
+export default function HomePage(props){
+    return(
+        <div>
+            Hello my name is {props.name}
+        </div>
+    )
+}
