4868426a73fba230cbb01e4f14cf2691f83b6c5d
ArmaanKatyal <armaankatyal3@gmail.com>
2021-11-13 18:49:33 -0700

    added the async and await to the routes

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,26 @@
+import flask
 from flask import Flask
 from flask import request
 from flask import url_for
 import sqlite3
 from flask import g
+from flask import render_template
+from flask import redirect
+import os
+import asyncio
+import platform
 
-DATABASE = '' # TODO: add path to the DATABASE
+path = ''
+system = platform.system()
+if system == 'Windows':
+    path = os.environ["userprofile"]
+    path += "\\Documents\\RAMDatabase\RAMdatabase.db"
+
+if system == 'Darwin':
+    path = os.path.expanduser('~/Documents')
+    path += "/RAMDatabase/RAMdatabase.db"
+
+DATABASE = path
 
 app = Flask(__name__)
 
@@ -20,29 +36,54 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+async def getAll(dbName):
+    return get_db().cursor().execute(f'select * from {dbName};').fetchall()
+
+async def getOne(dbName, id):
+    return get_db().cursor().execute(f'select * from {dbName} where id={id};').fetchone()
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 @app.route('/tips/allTips')
 def allTips():
-    return "AllTIps"
+    dataValue = asyncio.run(getAll('tips'))
+    return flask.jsonify(dataValue)
 
 @app.route('/tips/oneTip/<int:id>')
 def oneTip(id):
-    pass
+    dataValue = asyncio.run(getOne('tips', int(id)))
+    resultDict = {'id':dataValue[0], 'tip':dataValue[1]}
+    return flask.jsonify(resultDict)
 
 @app.route('/tips/randomTip')
 def randomTip():
     pass
 
+@app.route('/tips/create')
+def createTip():
+    return render_template('createTip.html')
+
+@app.route('/tips/create/process', methods=['POST'])
+def addTip():
+    if request.method == 'POST':
+        tip = request.form['tip']
+        db_conn = get_db()
+        db_conn.cursor().execute(f'insert into tips(TIP) values("{tip}");')
+        db_conn.commit()
+        return redirect('/tips/create')
+
 @app.route('/goals/allGoals')
 def allGoals():
-    pass
+    dataValue = asyncio.run(getAll('goals'))
+    return flask.jsonify(dataValue)
 
 @app.route('/goals/oneGoal/<int:id>')
 def oneGoal(id):
-    pass
+    dataValue = asyncio.run(getOne('goals', int(id)))
+    resultDict = {'id':dataValue[0], 'goal':dataValue[1]}
+    return flask.jsonify(resultDict)
 
 @app.route('/goals/randomGoal')
 def randomGoal():
@@ -52,28 +93,61 @@ def randomGoal():
 def suggestedGoal(userID):
     pass
 
+@app.route('/goals/create')
+def createGoal():
+    return render_template('createGoal.html')
+
+@app.route('/goals/create/process', methods=['POST'])
+def addGoal():
+    if request.method == 'POST':
+        goal = request.form['Goal']
+        db_conn = get_db()
+        db_conn.cursor().execute(f'insert into goals(GOAL) values("{goal}");')
+        db_conn.commit()
+        return redirect('/goals/create')
+
 @app.route('/user/get/<int:userID>')
 def getUser(userID):
+    # TODO : Get the user from the Database and return it in JSON format
     pass
 
 @app.route('/user/add', methods=['GET','POST'])
 def addUser():
     if request.method() == 'POST':
+        userID = request.json['userID']
+        # TODO : Add the user to the database when the POST request is made
         return
     else:
         return
 
 @app.route('/questions/allQuestions')
 def allQuestions():
-    pass
+    dataValue = asyncio.run(getAll('questions'))
+    return flask.jsonify(dataValue)
 
-@app.route('/questions/oneQuestion/<int:day>')
-def oneQuestion(day):
-    pass
+@app.route('/questions/oneQuestion/<int:id>')
+def oneQuestion(id):
+    dataValue = asyncio.run(getOne('questions', int(id)))
+    resultDict = {"id":dataValue[0], "question": dataValue[2]}
+    return flask.jsonify(resultDict)
 
 @app.route('/questions/randomQuestion')
 def randomQuestion():
     pass
+
+@app.route('/questions/create')
+def createQuestion():
+    return render_template('createQuestion.html')
+
+@app.route('/questions/create/process', methods=['POST'])
+def addQuestion():
+    if request.method == 'POST':
+        day = request.form['day']
+        question = request.form['question']
+        db_conn = get_db()
+        db_conn.cursor().execute(f'insert into questions(DAY, QUESTION) values("{day}", "{question}");')
+        db_conn.commit()
+        return redirect('/questions/create')
 
 if __name__ == '__main__':
     app.run()
---
+++ backend/makeDatabase.py
@@ -0,0 +1,38 @@
+import os
+import sqlite3
+import platform
+
+path = ''
+system = platform.system()
+
+if system == 'Windows':
+    path = os.environ["userprofile"]
+    try:
+        os.mkdir(path+"\\Documents\\RAMDatabase")
+    except FileExistsError:
+        pass
+if system == 'Darwin':
+    path = os.path.expanduser('~/Documents')
+    try:
+        os.mkdir(path+"/RAMDatabase")
+    except FileExistsError:
+        pass
+
+con = ''
+
+if system == 'Windows':
+    con = sqlite3.connect(path+"\\Documents\\RAMDatabase\RAMdatabase.db")
+if system == 'Darwin':
+    con = sqlite3.connect(path+"/Documents/RAMDatabase/RAMdatabase.db")
+
+print("Connection Successful")
+cur = con.cursor()
+
+#Creating Tables
+con.execute("create table if not exists tips(ID INTEGER PRIMARY KEY AUTOINCREMENT, TIP TEXT);")
+con.execute("create table if not exists goals(ID INTEGER PRIMARY KEY AUTOINCREMENT, GOAL TEXT);")
+con.execute("create table if not exists users(ID INTEGER PRIMARY KEY AUTOINCREMENT, USERID VARCHAR(20));")
+con.execute("create table if not exists questions(ID INTEGER PRIMARY KEY AUTOINCREMENT, DAY INTEGER, QUESTION TEXT);")
+
+cur.close()
+print("Tables created Successfully")
