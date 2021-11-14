a7a06153eaf2d16eeb1413ffa5a856eb6ad66cf3
mansooranis <mansooranis60@gmail.com>
2021-11-13 20:40:49 -0700

    added homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,6 @@ import HomePage from './Pages/HomePage/Components/HomePage';
 function App() {
   return (
     <div>
-      <h3 className="h">Hello world</h3>
-      <HomePage name = "mansoor"/>
-      <HomePage name= "armaan"/>
-      <HomePage/>
       <HomePage/>
     </div>
   );
